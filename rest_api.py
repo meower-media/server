d06e2a081e4c5a372bfbc9cc57131d1ed298340d
Tnix <61485190+tnix100@users.noreply.github.com>
2022-04-20 13:30:04 +1200

    Update rest_api.py

--- rest_api.py
+++ rest_api.py
@@ -34,6 +34,8 @@ def fetch_post_from_storage(post_id):
         
         if result:
 	    if payload["post_origin"] != "home":
+		payload["post_origin"] = None
+		payload["t"] = None
 		payload["u"] = None
 		payload["p"] = None
             elif payload["isDeleted"]:
