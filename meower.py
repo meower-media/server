dbc86a47a6f9ee8aba2cf218eabe89b6c20a3f75
ShowierData9978 <mellfang36@outlook.com>
2022-10-27 21:04:28 -0500

    fixed group chat names not being censored

--- meower.py
+++ meower.py
@@ -6,6 +6,8 @@ import os
 from dotenv import load_dotenv
 import requests
 
+from better_profanity import profanity
+
 load_dotenv()  # take environment variables from .env.
 
 class Meower:
@@ -1302,6 +1304,9 @@ class Meower:
                 # Post not found
                 self.returnCode(client = client, code = "IDNotFound", listener_detected = listener_detected, listener_id = listener_id)
         else:
+
+
+
             # Not authenticated
             self.returnCode(client = client, code = "Refused", listener_detected = listener_detected, listener_id = listener_id)
     
@@ -1310,6 +1315,7 @@ class Meower:
         if self.supporter.isAuthenticated(client):
             if type(val) == str:
                 if not len(val) > 20:
+					val = profanity.censor(val)
                     if not self.filesystem.does_item_exist("chats", val):
                         result = self.filesystem.create_item("chats", str(uuid.uuid4()), {"nickname": val, "owner": client, "members": [client]})
                         if result:
