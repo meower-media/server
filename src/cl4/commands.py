d054a47cc3d8a3265bd3cbea17d484076a9280a6
Tnix <tnix@meower.org>
2023-02-03 11:07:30 +1300

    Fix #146

--- src/cl4/commands.py
+++ src/cl4/commands.py
@@ -57,8 +57,8 @@ class CL4Commands:
                 "session_id": session.id,
                 "user": session.user.client,
                 "chats": [chat.public for chat in chats.get_active_chats(session.user)],
-                "following": session.user.get_following(),
-                "blocked": session.user.get_blocked(),
+                "following": session.user.get_following_ids(),
+                "blocked": session.user.get_blocked_ids(),
                 "infractions": [infraction.client for infraction in infractions.get_user_infractions(session.user)],
                 "time_taken": int((time.time()-timer_start)*1000)
             },
