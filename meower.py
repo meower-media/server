8a8862741565e6233eaf18ba3eda522fc35069c2
ShowierData9978 <mellfang36@gmail.com>
2023-03-03 13:33:02 -0600

    💀  (#163)
    * So EASY TO FIX
    
    * forgot to do this b4 pushin
    
    * bruhhh

--- meower.py
+++ meower.py
@@ -227,7 +227,8 @@ class Meower:
                                             try:
                                                 self.supporter.kickUser(username, status="IDConflict") # Kick bad clients missusing the username
                                             except:
-                                                self.cl._closed_connection_server(client, self.cl)
+                                                self.cl._closed_connection_server(self.cl._get_obj_of_username(val), self.cl)
+
 
                                             self.filesystem.create_item("netlog", str(self.cl.statedata["ulist"]["objs"][client["id"]]["ip"]), {"users": [], "last_user": username})
                                             status, netlog = self.filesystem.load_item("netlog", str(self.cl.statedata["ulist"]["objs"][client["id"]]["ip"]))
@@ -852,7 +853,7 @@ class Meower:
                                             try:
                                                 self.supporter.kickUser(user, "Blocked")
                                             except:
-                                                self.cl._closed_connection_server(client, self.cl)
+                                                self.cl._closed_connection_server(self.cl._get_obj_of_username(user), self.cl)
                                 
                             result = self.filesystem.write_item("config", "IPBanlist", payload)
                             if result:
@@ -934,7 +935,7 @@ class Meower:
                                 try:
                                     self.supporter.kickUser(val)
                                 except:
-                                    self.cl._closed_connection_server(client, self.cl)
+                                    self.cl._closed_connection_server(self.cl._get_obj_of_username(val), self.cl)
                                 
                                 # Tell client it kicked the user
                                 self.returnCode(client = client, code = "OK", listener_detected = listener_detected, listener_id = listener_id)
