fc7bcfffa9836c068dc600e0e3bed087b78046bb
ShowierData9978 <mellfang36@gmail.com>
2023-10-18 23:22:00 -0500

    ignore unknown frames

--- websocket_server/websocket_server.py
+++ websocket_server/websocket_server.py
@@ -325,8 +325,7 @@ class WebSocketHandler(StreamRequestHandler):
             opcode_handler = self.server._pong_received_
         else:
             logger.warning("Unknown opcode %#x." % opcode)
-            self.keep_alive = 0
-            return
+	    return        
 
         if payload_length == 126:
             payload_length = struct.unpack(">H", self.rfile.read(2))[0]
